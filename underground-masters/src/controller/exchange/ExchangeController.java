84433ffa5ef0dbf93d532cd0801d93ac166a5f27
hagnoykmik <ymj02148@naver.com>
2025-07-20 17:14:41 +0900

    FEAT: 교환글 목록 조회 기능 추가

--- underground-masters/src/controller/exchange/ExchangeController.java
+++ underground-masters/src/controller/exchange/ExchangeController.java
@@ -1,24 +1,76 @@
 package controller.exchange;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import model.exchange.ExchangeDAO;
+import model.exchange.ExchangeDTO;
 
 public class ExchangeController implements Initializable {
-	// ExchangeListView.fxml에서 찾아서 등록
-//	@FXML private Button registerExchangeBtn;
 	
+	// ExchangeListView.fxml에서 찾아서 등록
+	@FXML private TableView<ExchangeDTO> exchangeTableView;
+	@FXML private TableColumn<ExchangeDTO, String> talentName;
+	@FXML private TableColumn<ExchangeDTO, String> memberName;
+	@FXML private TableColumn<ExchangeDTO, Date> createAt;
+	
+	// 부서 데이터를 담을 ObservableList -> 테이블 뷰에 넣으려면 ObservableList 써야함 (JavaFX 컨트롤과 자동 동기화가 가능)
+    private ObservableList<ExchangeDTO> exchangeList = null;
+	
+    /**
+     * 교환글 목록 조회
+     */
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-//		registerExchangeBtn.setOnAction(event -> onShowRegisterExchangeBtnPopup(event));	
+	public void initialize(URL location, ResourceBundle resources) { // 자동 호출
+		
+		// 1단계: 컬럼 매핑(바인딩)
+		StringProperty memberNameById = new SimpleStringProperty("김경아"); // TODO: memberId로 memberName가져오기
+		
+		// TableView 컬럼 ↔ DTO 속성(Property) 매핑만 하는 역할
+		talentName.setCellValueFactory(cellData -> cellData.getValue().titleProperty());
+//		memberName.setCellValueFactory(cellData -> cellData.getValue().exchangeIdProperty().asObject()); //TODO: memberId로 memberName가져오기
+		memberName.setCellValueFactory(cellData -> memberNameById);
+		createAt.setCellValueFactory(cellData -> cellData.getValue().createAtProperty());
+		
+		// 2단계: DB에서 데이터 조회 후 TableView에 세팅
+	    try {
+	        ObservableList<ExchangeDTO> list = ExchangeDAO.findExchangeList();
+	        exchangeTableView.setItems(list);
+	    } catch(Exception e) {
+	        e.printStackTrace();
+	    }
+	    
+	}
+	
+	
+	// 교환글 목록을 Table View에 매핑하기
+	@FXML
+	private void populateEmployee(ObservableList<ExchangeDTO> exchangeList) throws ClassNotFoundException {
+		exchangeTableView.setItems(exchangeList); // 실제 데이터를 TableView에 set
+	}
+	
+
+	// 교환글 조회
+	@FXML
+	private void searchExchanges(ActionEvent event) throws ClassNotFoundException, SQLException {
+		exchangeList = ExchangeDAO.findExchangeList(); // DAO에서 DB 쿼리 수행해서 교환글 목록 불러옴
+		populateEmployee(exchangeList); // TableView에 보여줌
+		
 	}
 	
 	/*
--- underground-masters/src/model/exchange/ExchangeDAO.java
+++ underground-masters/src/model/exchange/ExchangeDAO.java
@@ -3,11 +3,49 @@ package model.exchange;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.border.TitledBorder;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import util.DBUtil;
 
 public class ExchangeDAO {
+	
+	/**
+	 * 교환글 목록 조회
+	 */
+	public static ObservableList<ExchangeDTO> findExchangeList() throws ClassNotFoundException, SQLException {
+		ObservableList<ExchangeDTO> exchangeList = FXCollections.observableArrayList(); // 교환글 담을 리스트
+		
+		// DB에서 교환글 리스트 불러오기
+		String selectStmt = "SELECT TITLE, MEMBER_ID, CREATE_AT FROM MASTERS.EXCHANGE";
+		ResultSet resultSet = DBUtil.dbExecuteQuery(selectStmt);
+		
+		// resultSet에 담겨있는 교환글 리스트를 exchangeList에 담기
+		exchangeList = getExchangeDTOFromResultSet(resultSet);
+		
+		return exchangeList;
+	}
+	
+	// ResultSet 한 행을 ExchangeDTO 객체로 변환
+	public static ObservableList<ExchangeDTO> getExchangeDTOFromResultSet(ResultSet resultSet) throws SQLException {
+		ObservableList<ExchangeDTO> exchangeList = FXCollections.observableArrayList(); // 교환글 담을 리스트
+
+		while (resultSet.next()) { // resultSet 순회하면서 exchangeDTO로 담기
+			ExchangeDTO exchangeDTO = ExchangeDTO.builder()
+					.title(new SimpleStringProperty(resultSet.getString("TITLE")))
+					.memberId(new SimpleIntegerProperty(resultSet.getInt("MEMBER_ID")))
+					.createAt(new SimpleObjectProperty<>(resultSet.getDate("CREATE_AT")))
+					.build();
+			
+			exchangeList.add(exchangeDTO); // ObservableList에 담기
+		}
+		return exchangeList;
+	}
+	
 	/**
 	 * 재능 리스트 불러오기
 	 * @throws SQLException 
@@ -17,24 +55,26 @@ public class ExchangeDAO {
 		ObservableList<String> talentList = FXCollections.observableArrayList(); // 내가 보유하고 있는 재능을 담을 리스트
 		
 		int memberId = 1; // TODO: 임시 데이터(현재 접속한 ID값으로 바꾸기)
-		String selectStmt = "SELECT NAME FROM MASTERS.TALENT WHERE MEMBER_ID = ?";
-		ResultSet resultSet = DBUtil.dbExecuteQuery(selectStmt, memberId); // DB에서 재능 리스트 불러오기
 		
-		talentList = getTalentDTOFromResultSet(resultSet); // resultSet에 담겨있는 재능 리스트를 talentList에 담기
+		// DB에서 재능 리스트 불러오기
+		String selectStmt = "SELECT NAME FROM MASTERS.TALENT WHERE MEMBER_ID = ?";
+		ResultSet resultSet = DBUtil.dbExecuteQuery(selectStmt, memberId);
+		
+		// resultSet에 담겨있는 재능 리스트를 talentList에 담기
+		talentList = getTalentDTOFromResultSet(resultSet); 
 		
 		return talentList;
 	}
 	
-	/**
-	 * ResultSet 한 행을 TalentDTO 객체로 변환 
-	 * @throws SQLException 
-	 */
+	// ResultSet 한 행을 TalentDTO 객체로 변환
 	public static ObservableList<String> getTalentDTOFromResultSet(ResultSet resultSet) throws SQLException {
 		ObservableList<String> talentList = FXCollections.observableArrayList(); // 내가 보유하고 있는 재능을 담을 리스트
-		String name = "";
+		
+		String talentName = ""; // 재능 이름
 		while (resultSet.next()) {
-			name = resultSet.getString("NAME");
-			talentList.add(name);
+			talentName = resultSet.getString("NAME");
+			
+			talentList.add(talentName); // ObservableList에 담기
 		}
 		return talentList;
 	}
@@ -47,6 +87,8 @@ public class ExchangeDAO {
 	public static void createExchange(String selectedTalent, String description) throws ClassNotFoundException, SQLException {
 		String insertStmt = "{ call create_exchange(?, ?, ?) }"; // 재능, 상세설명, 멤버id
 		int memberId = 1; //TODO: 임시 데이터(현재 접속한 ID값으로 바꾸기)
+		
+		// DB에 교환글 등록하기
 		DBUtil.dbExecuteUpdate(insertStmt, selectedTalent, description, memberId);
 	}
 }
