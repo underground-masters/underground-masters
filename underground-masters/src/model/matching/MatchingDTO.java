a970b1f3c70e88cd62f9d3af7245a85bd895a6ed
ycson <ymj06222@naver.com>
2025-07-21 17:55:05 +0900

    FEAT: matching_receive 받은 목록 조회 기능 추가

--- underground-masters/src/model/matching/MatchingDTO.java
+++ underground-masters/src/model/matching/MatchingDTO.java
@@ -1,5 +1,13 @@
 package model.matching;
 
-public class MatchingDTO {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class MatchingDTO {
+    private String talentName;     // 상대 재능
+    private String memberName;     // 상대 고수
+    private String status;         // 상태 (대기, 수락, 거절)
+    private String requestDate;    // 신청 날짜
 }
